f352cccdf1181c91caf6ca2554211257610e5b0a
Héctor Isahi Ochoa <lacasademiamigochanel@gmail.com>
2024-11-05 08:25:19 -0600

    Implement order management page with SWR for data fetching; add refresh functionality and API route for pending orders

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,37 +1,34 @@
+"use client";
+import useSWR from "swr";
 import OrderCard from "@/components/order/OrderCard";
 import Heading from "@/components/ui/Heading";
-import { prisma } from "@/src/lib/prisma";
-import React from "react";
+import { OrderWithProducts } from "@/src/types";
 
-const getPendingOrders = async () => {
-  const orders = await prisma.order.findMany({
-    where: {
-      status: false,
-    },
-    include: {
-      orderProducts: {
-        include: {
-          product: true,
-        },
-      }
-    }
-  });
-  return orders;
-};
+export default function OrderPage() {
+  const url = "/admin/orders/api";
+  const fetcher = (url : string) => fetch(url).then((res) => res.json());
+  const { data: orders, isLoading, error } = useSWR<OrderWithProducts[]>(url, fetcher, { refreshInterval: 60000 });
 
-export default async function OrderPage() {
-  const orders = await getPendingOrders();
-  console.log(orders);
-  return (
+  const refreshOrders = () => {
+    window.location.reload();
+  };
+  if(orders) return (
     <>
       <Heading>Administra las ordenes</Heading>
+      <form action={refreshOrders}>
+        <input
+          type="submit"
+          value="Actualizar ordenes"
+          className="bg-amber-400 w-full lg:w-auto text-xl px-10 py-3 text-center font-bold cursor-pointer"
+        />
+      </form>
       {orders.length ? (
         <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5">
           {orders.map((order) => (
             <OrderCard key={order.id} order={order} />
           ))}
         </div>
-      )  : (
+      ) : (
         <p>No hay ordenes pendientes</p>
       )}
     </>
